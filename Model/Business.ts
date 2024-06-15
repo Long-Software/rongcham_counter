dbd7986abe0036792888bbad74e1ee2df89c5191
Long-Software <senglongsoftware@gmail.com>
2024-06-15 08:27:58 +0700

    default

--- Model/Business.ts
+++ Model/Business.ts
@@ -6,7 +6,7 @@ const create = async (data: Prisma.BusinessCreateInput) => {
   const Business = z.object({ email: z.string().email() })
   try {
     Business.parse(data)
-    prisma.$disconnect()
+    // prisma.$disconnect()
     const business = await prisma.business.create({ data })
     return { success: true, message: 'success', data: business }
   } catch (error) {
@@ -15,7 +15,7 @@ const create = async (data: Prisma.BusinessCreateInput) => {
 }
 
 const all = async (args?: Prisma.BusinessFindManyArgs) => {
-  prisma.$disconnect()
+  // prisma.$disconnect()
   return await prisma.business.findMany(args)
 }
 
--- Model/Category.ts
+++ Model/Category.ts
@@ -3,13 +3,13 @@ import { Prisma } from '@prisma/client'
 import { z } from 'zod'
 
 const all = async (args?: Prisma.CategoryFindManyArgs) => {
-  await prisma.$disconnect()
-  await prisma.$connect()
+  // await prisma.$disconnect()
+  // await prisma.$connect()
   return await prisma.category.findMany(args)
 }
 const find = async (args?: Prisma.CategoryFindFirstArgs) => {
-  await prisma.$disconnect()
-  await prisma.$connect()
+  // await prisma.$disconnect()
+  // await prisma.$connect()
   return await prisma.category.findFirst(args)
 }
 
