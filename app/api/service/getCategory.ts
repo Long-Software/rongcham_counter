f31d8cf66c9d3550c2e4491a04203bf483be6718
Long-Software <senglongsoftware@gmail.com>
2024-06-15 09:37:38 +0700

    default

--- app/api/service/getCategory.ts
+++ app/api/service/getCategory.ts
@@ -4,7 +4,7 @@ import { api } from '../api'
 
 export const getCategory = async (business_token: string): Promise<Category[]> => {
   try {
-    const res = await api.get(`/categories?token=${business_token}`)
+    const res = await api.get(`categories?token=${business_token}`)
     if (res.data.status == 'error') throw new Error(res.data.message)
     return res.data.data
   } catch (error) {
--- app/api/service/getQueue.ts
+++ app/api/service/getQueue.ts
@@ -4,7 +4,7 @@ import { api } from '../api'
 
 export const getQueue = async (business_token: string): Promise<QueueInfo[]> => {
   try {
-    const res = await api.get(`/queues?token=${business_token}`)
+    const res = await api.get(`queues?token=${business_token}`)
     if (res.data.status == 'error') throw new Error(res.data.message)
     return res.data.data
   } catch (error) {
