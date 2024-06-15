482ef8bc5b731acc8a33c5d0f743c57d83a48465
Long-Software <senglongsoftware@gmail.com>
2024-06-15 09:34:58 +0700

    default

--- app/api/service/getQueue.ts
+++ app/api/service/getQueue.ts
@@ -1,11 +1,10 @@
-'use server'
+// 'use server'
 import axios from 'axios'
 import { api } from '../api'
 
 export const getQueue = async (business_token: string): Promise<QueueInfo[]> => {
   try {
     const res = await api.get(`/queues?token=${business_token}`)
-    // const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/queues?token=${business_token}`)
     if (res.data.status == 'error') throw new Error(res.data.message)
     return res.data.data
   } catch (error) {
