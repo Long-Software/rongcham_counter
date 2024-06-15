3bc7f3c0a6f6c2e332162a52c9854fb727d233bd
Long-Software <senglongsoftware@gmail.com>
2024-06-15 08:14:42 +0700

    default

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -4,13 +4,18 @@ import Category from '@/Model/Category'
 import { NextRequest, NextResponse } from 'next/server'
 
 export const GET = async (req: NextRequest) => {
-  const token = req.nextUrl.searchParams.get('token') ?? ''
+  try {
+    const token = req.nextUrl.searchParams.get('token') ?? ''
 
-  const categories = await Category.all({
-    where: { business_token: token }
-  })
-  if (categories) {
-    return responseWithSuccess( 'fetch successfully', categories, 200)
+    const categories = await Category.all({
+      where: { business_token: token }
+    })
+    if (categories) {
+      return responseWithSuccess('fetch successfully', categories, 200)
+    }
+    return responseWithError('unauthorize', null)
+  } catch (error) {
+    console.log(error)
+    return responseWithError('unauthorize', null)
   }
-  return responseWithError('unauthorize', null)
 }
--- app/api/service/getCategory.ts
+++ app/api/service/getCategory.ts
@@ -5,9 +5,7 @@ import { api } from '../api'
 
 export const getCategory = async (business_token: string): Promise<Category[]> => {
   try {
-    const res = await api.get(`/categories?token=${business_token}`, {
-      responseType: 'json'
-    })
+    const res = await api.get(`/categories?token=${business_token}`)
     // const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/categories?token=${business_token}`)
     if (res.data.status == 'error') throw new Error(res.data.message)
     return res.data.data
