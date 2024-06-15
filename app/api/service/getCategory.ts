d987fd6ba076e534d5dd42e7b8ddb81ded6987d2
Long-Software <senglongsoftware@gmail.com>
2024-06-15 07:34:01 +0700

    default

--- app/api/service/getCategory.ts
+++ app/api/service/getCategory.ts
@@ -1,10 +1,12 @@
 'use server'
 import { Category, Prisma } from '@prisma/client'
 import axios from 'axios'
+import { api } from '../api'
 
 export const getCategory = async (business_token: string): Promise<Category[]> => {
   try {
-    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/categories?token=${business_token}`)
+    const res = await api.get(`/categories?token=${business_token}`)
+    // const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/categories?token=${business_token}`)
     if (res.data.status == 'error') throw new Error(res.data.message)
     return res.data.data
   } catch (error) {
