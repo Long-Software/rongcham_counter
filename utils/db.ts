0e23cab04f084838f8f207411ee68377dea3b2c6
Long-Software <senglongsoftware@gmail.com>
2024-06-15 07:55:47 +0700

    default

--- utils/db.ts
+++ utils/db.ts
@@ -10,8 +10,9 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined
 }
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
-
-export default prisma
+let prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV === 'production') prisma = prismaClientSingleton()
+
+export default prisma
